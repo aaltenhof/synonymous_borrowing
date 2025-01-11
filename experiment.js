fd02f1a9b281b9006efb308fb3aec811f1f2deba
cbergey <clairebergey@gmail.com>
2025-01-10 19:28:57 -0600

    port blah

--- experiment.js
+++ experiment.js
@@ -1,7 +1,7 @@
 // Declare participant_id at the top
 let participant_id;
 let prolific_id;
-const PORT = process.env.PORT || 5000
+const PORT = app.get('port') || 5000
 novel_words = ["tinches", "nefts", "bines", "palts"]
 if (Math.floor(Math.random() * 2) == 0) {
     condition = "novel_word_condition"
