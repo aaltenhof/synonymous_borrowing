fa183acd8a62101f8f3ae9c9d8a425b70cd38c47
cbergey <clairebergey@gmail.com>
2025-01-10 19:57:25 -0600

    port dotenv

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const cors = require('cors');
-require('dotenv').config();
+
 const PORT = process.env.PORT || 5000
 
 
