6416c9992da87c3bb8af1005c89f158b6a5af084
cbergey <clairebergey@gmail.com>
2025-01-10 19:33:50 -0600

    blah

--- experiment.js
+++ experiment.js
@@ -1,7 +1,7 @@
 // Declare participant_id at the top
 let participant_id;
 let prolific_id;
-import { PORT } from './server.js'
+
 novel_words = ["tinches", "nefts", "bines", "palts"]
 if (Math.floor(Math.random() * 2) == 0) {
     condition = "novel_word_condition"
