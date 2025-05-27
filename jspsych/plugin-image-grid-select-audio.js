87c72e1db085d7acff67ea913e0fd3334b5d3a1c
cbergey <clairebergey@gmail.com>
2025-05-26 23:41:11 -0700

    autobind

--- jspsych/plugin-image-grid-select-audio.js
+++ jspsych/plugin-image-grid-select-audio.js
@@ -8,6 +8,54 @@ var jsPsychImageGridSelectAudio = (function (jspsych) {
         }
     }
 
+    function getDefaultExportFromCjs (x) {
+		return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, 'default') ? x['default'] : x;
+	}
+
+	// Gets all non-builtin properties up the prototype chain
+	const getAllProperties = object => {
+		const properties = new Set();
+
+		do {
+			for (const key of Reflect.ownKeys(object)) {
+				properties.add([object, key]);
+			}
+		} while ((object = Reflect.getPrototypeOf(object)) && object !== Object.prototype);
+
+		return properties;
+	};
+
+	var autoBind = (self, {include, exclude} = {}) => {
+		const filter = key => {
+			const match = pattern => typeof pattern === 'string' ? key === pattern : pattern.test(key);
+
+			if (include) {
+				return include.some(match);
+			}
+
+			if (exclude) {
+				return !exclude.some(match);
+			}
+
+			return true;
+		};
+
+		for (const [object, key] of getAllProperties(self.constructor.prototype)) {
+			if (key === 'constructor' || !filter(key)) {
+				continue;
+			}
+
+			const descriptor = Reflect.getOwnPropertyDescriptor(object, key);
+			if (descriptor && typeof descriptor.value === 'function') {
+				self[key] = self[key].bind(self);
+			}
+		}
+
+		return self;
+	};
+
+	var autoBind$1 = /*@__PURE__*/getDefaultExportFromCjs(autoBind);
+
     const info = {
         name: 'image-grid-select-audio',
         parameters: {
