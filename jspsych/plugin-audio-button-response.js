12639bff9aa26b3f4a76a06f0e688b2c6f5534aa
cbergey <clairebergey@gmail.com>
2025-05-26 23:02:31 -0700

    indenting

--- jspsych/plugin-audio-button-response.js
+++ jspsych/plugin-audio-button-response.js
@@ -91,5 +91,56 @@ var jsPsychAudioButtonResponse=function(e){
 				for(s=0;s<t.choices.length;s++){
 					var c=a[s].replace(/%choice%/g,t.choices[s]);
 					u+='<div class="jspsych-audio-button-response-button" style="cursor: pointer; display: inline-block; margin:'+t.margin_vertical+" "+t.margin_horizontal+'" id="jspsych-audio-button-response-button-'+s+'" data-choice="'+s+'">'+c+"</div>"}u+="</div>",null!==t.prompt&&(u+=t.prompt),e.innerHTML=u,t.response_allowed_while_playing?p():d(),r=performance.now(),null!==o?(r=o.currentTime,i.audio.start(r)):i.audio.play(),null!==t.trial_duration&&i.jsPsych.pluginAPI.setTimeout((function(){l()}),t.trial_duration),n()},l=function n(){i.jsPsych.pluginAPI.clearAllTimeouts(),null!==o?i.audio.stop():i.audio.pause(),i.audio.removeEventListener("ended",n),i.audio.removeEventListener("ended",p);
-						var r={rt:s.rt,stimulus:t.stimulus,response:s.button};e.innerHTML="",i.jsPsych.finishTrial(r),a()};function c(e){!function(e){var n=performance.now(),a=Math.round(n-r);null!==o&&(n=o.currentTime,a=Math.round(1e3*(n-r))),s.button=parseInt(e),s.rt=a,d(),t.response_ends_trial&&l()}(e.currentTarget.getAttribute("data-choice"))}function d(){for(var e=document.querySelectorAll(".jspsych-audio-button-response-button"),t=0;t<e.length;t++){var n=e[t].querySelector("button");n&&(n.disabled=!0),e[t].removeEventListener("click",c)}}function p(){for(var e=document.querySelectorAll(".jspsych-audio-button-response-button"),t=0;t<e.length;t++){var n=e[t].querySelector("button");n&&(n.disabled=!1),e[t].addEventListener("click",c)}}return new Promise((function(e){a=e}))}},{key:"simulate",value:function(e,t,n,a){"data-only"==t&&(a(),this.simulate_data_only(e,n)),"visual"==t&&this.simulate_visual(e,n,a)}},{key:"create_simulation_data",value:function(e,t){var n={stimulus:e.stimulus,rt:this.jsPsych.randomization.sampleExGaussian(500,50,1/150,!0),response:this.jsPsych.randomization.randomInt(0,e.choices.length-1)},a=this.jsPsych.pluginAPI.mergeSimulationData(n,t);return this.jsPsych.pluginAPI.ensureSimulationDataConsistency(e,a),a}},{key:"simulate_data_only",value:function(e,t){var n=this.create_simulation_data(e,t);this.jsPsych.finishTrial(n)}},{key:"simulate_visual",value:function(e,t,n){var a=this,r=this.create_simulation_data(e,t),i=this.jsPsych.getDisplayElement(),o=function(){null!==r.rt&&a.jsPsych.pluginAPI.clickTarget(i.querySelector('div[data-choice="'.concat(r.response,'"] button')),r.rt)};this.trial(i,e,(function(){n(),e.response_allowed_while_playing?o():a.audio.addEventListener("ended",o)}))}}])&&t(n.prototype,a),r&&t(n,r),Object.defineProperty(n,"prototype",{writable:!1}),e}();return a.info=n,a}(jsPsychModule);
+					var r={
+						rt:s.rt,
+						stimulus:t.stimulus,
+						response:s.button
+					};
+					e.innerHTML="",i.jsPsych.finishTrial(r),a()
+				}; 
+				function c(e){
+					!function(e){
+						var n=performance.now(),a=Math.round(n-r);
+						null!==o&&(n=o.currentTime,a=Math.round(1e3*(n-r))),s.button=parseInt(e),s.rt=a,d(),t.response_ends_trial&&l()
+					}(e.currentTarget.getAttribute("data-choice"))
+				}
+				function d(){
+					for(var e=document.querySelectorAll(".jspsych-audio-button-response-button"),t=0;t<e.length;t++){
+						var n=e[t].querySelector("button");
+						n&&(n.disabled=!0),e[t].removeEventListener("click",c)
+					}
+				}
+				function p(){
+					for(var e=document.querySelectorAll(".jspsych-audio-button-response-button"),t=0;t<e.length;t++){
+						var n=e[t].querySelector("button");
+						n&&(n.disabled=!1),e[t].addEventListener("click",c)
+					}
+				}
+				return new Promise((function(e){a=e}))
+			}
+		},
+		{key:"simulate",value:function(e,t,n,a){
+			"data-only"==t&&(a(),this.simulate_data_only(e,n)),"visual"==t&&this.simulate_visual(e,n,a)
+		}},
+		{key:"create_simulation_data",value:function(e,t){
+			var n={
+				stimulus:e.stimulus,
+				rt:this.jsPsych.randomization.sampleExGaussian(500,50,1/150,!0),
+				response:this.jsPsych.randomization.randomInt(0,e.choices.length-1)
+			},
+			a=this.jsPsych.pluginAPI.mergeSimulationData(n,t);
+			return this.jsPsych.pluginAPI.ensureSimulationDataConsistency(e,a),a
+		}},
+		{key:"simulate_data_only",value:function(e,t){
+			var n=this.create_simulation_data(e,t);
+			this.jsPsych.finishTrial(n)
+		}},
+		{key:"simulate_visual",value:function(e,t,n){
+			var a=this,r=this.create_simulation_data(e,t),i=this.jsPsych.getDisplayElement(),o=function(){
+				null!==r.rt&&a.jsPsych.pluginAPI.clickTarget(i.querySelector('div[data-choice="'.concat(r.response,'"] button')),r.rt)
+			};
+			this.trial(i,e,(function(){
+				n(),e.response_allowed_while_playing?o():a.audio.addEventListener("ended",o)
+			}))
+		}}])&&t(n.prototype,a),r&&t(n,r),Object.defineProperty(n,"prototype",{writable:!1}),e}();return a.info=n,a}(jsPsychModule);
 //# sourceMappingURL=index.browser.min.js.map
